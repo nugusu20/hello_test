b51ced544a4dfc796f04a26c6f2ee4065d3daef0
nugusu20 <120416067+nugusu20@users.noreply.github.com>
2024-06-17 22:02:38 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,6 +5,6 @@ def factorial(n):
          return n * factorial(n - 1)
 number = 5
 result = factorial(number)
-print (f"the fatorial of nimber is (result).")
+print (f"the factorial of number is (result).")
          
         
